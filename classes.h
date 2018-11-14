5d3c83ae6033b5bdada9db6eb5e7d0276ed1edcb
DenPhoenix <43912143+DenPhoenix@users.noreply.github.com>
2018-11-14 10:55:11 +0200

    Create classes.h

---
+++ classes.h
@@ -0,0 +1,12 @@
+#ifndef WARD_6_CLASSES_H_INCLUDED
+#define WARD_6_CLASSES_H_INCLUDED
+
+class card {
+  int type;
+  int arg_type;
+  public:
+  card(int t);
+  void show();
+};
+
+#endif // WARD_6_CLASSES_H_INCLUDED
