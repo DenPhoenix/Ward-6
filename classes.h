13e9258d96fd70cd5140f80385d145c9cc195995
DenPhoenix <43912143+DenPhoenix@users.noreply.github.com>
2018-11-14 22:42:50 +0200

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,15 +1,30 @@
-#ifndef WARD_6_CLASSES_H_INCLUDED
-#define WARD_6_CLASSES_H_INCLUDED
-#include <string>
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
 
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <iostream>
+
+//*********************************************************//
 class card {
   int type;
   int arg_type;
   std::string name;
-  public:
+public:
   card(int t);
   void show();
   int get_arg_type();
 };
+//*********************************************************//
+class card_deck{
+    std::vector<card> deck;
+public:
+   card_deck(std::vector<int> init);
+   void resort();
+card operator [](int i);
+};
 
-#endif // WARD_6_CLASSES_H_INCLUDED
+#endif // CLASSES_H_INCLUDED
+//*********************************************************//
