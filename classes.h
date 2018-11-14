6fa2d6fd37a51ca273dae85945a8aa137d604a86
DenPhoenix <43912143+DenPhoenix@users.noreply.github.com>
2018-11-14 11:10:13 +0200

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,9 +1,11 @@
 #ifndef WARD_6_CLASSES_H_INCLUDED
 #define WARD_6_CLASSES_H_INCLUDED
+#include <string>
 
 class card {
   int type;
   int arg_type;
+  std::string name;
   public:
   card(int t);
   void show();
