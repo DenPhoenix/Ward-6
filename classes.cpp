8a94bab29b71a076836c693b8f16c8fbcb004dc6
DenPhoenix <43912143+DenPhoenix@users.noreply.github.com>
2018-11-14 11:20:05 +0200

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -3,7 +3,15 @@
 card::card(int t){
   type(t);
   switch t {
-    case 1: {arg_type=1;break;}
-    case 2: {arg_type=2;break;}
-    case 3: {arg_type=3;break;}
-    default:{std::cout<<"Error.This type don't exist.."<<std::endl;break;} }
+    case 1: {arg_type=1;
+             name="I am hangry...";break;}
+    case 2: {arg_type=2;
+             name=" ";break;}
+    case 3: {arg_type=3;
+             name="";break;}
+    default:{name="Error.This type don't exist..";break;} }
+                                                            }
+card::show(){
+  std::cout<<name<<std::endl;}
+
+int card::get_arg_type(){return arg_type;}
